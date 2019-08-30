b61a360b71f20fcc9c0568099ca0c27d83523258
kodai100 <kodai100@gmail.com>
2019-08-30 14:37:03 +0900

    クラス名変更

--- Assets/UnityTCP/Scripts/TCPServer.cs
+++ Assets/UnityTCP/Scripts/TCPServer.cs
@@ -1,43 +1,153 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using System.Text;
+using System.IO;
 
 namespace Kodai100.Tcp
 {
-
-    public class TCPServer : MonoBehaviour
+    
+    class TCPServer : IDisposable
     {
-
-        UnityTCP server;
-
-        public int openPort = 7000;
-        public OnMessageEvent OnMessage;
         
-        void Start()
+        IPEndPoint endpoint;
+
+        TcpListener listener;
+        HashSet<TcpClient> clients = new HashSet<TcpClient>();
+
+        SynchronizationContext mainContext;
+        volatile bool acceptLoop = true;
+        
+        OnMessageEvent OnMessage;
+
+
+        public TCPServer(IPEndPoint endpoint, OnMessageEvent onMessage)
         {
+            this.endpoint = endpoint ?? throw new ArgumentNullException("endpoint should not be null");
 
-            server = new UnityTCP(new IPEndPoint(IPAddress.Any, openPort), OnMessage);
+            // Set Unity main thread
+            mainContext = SynchronizationContext.Current;
 
-            var t = server.Listen();
-            if (t.IsFaulted) t.Wait();
+            OnMessage = onMessage;
+        }
+        
+        public async Task Listen()
+        {
+            lock (this)
+            {
+                if (listener != null)
+                    throw new InvalidOperationException("Already started");
+
+                acceptLoop = true;
+                listener = new TcpListener(endpoint);
+            }
+
+            listener.Start();
+
+            while (acceptLoop)
+            {
+                try
+                {
+                    var client = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+                    
+                    var _ = Task.Run(() => OnConnectClient(client));
+
+                }
+                catch (ObjectDisposedException e)
+                {
+                    // thrown if the listener socket is closed
+                }
+                catch (SocketException e)
+                {
+                    // Some socket error
+                }
+            }
         }
 
 
-        private void OnDisable()
+
+        public void Stop()
         {
-            server.Stop();
+
+            lock (this)
+            {
+
+                if (listener == null)
+                    throw new InvalidOperationException("Not started");
+
+                acceptLoop = false;
+
+                listener.Stop();
+                listener = null;
+
+            }
+
+
+            lock (clients)
+            {
+                foreach (var c in clients)
+                {
+                    c.Close();
+                }
+            }
+
         }
 
+        async Task OnConnectClient(TcpClient client)
+        {
+            var clientEndpoint = client.Client.RemoteEndPoint;
+
+            Debug.Log($"<color=yellow>Established</color> : {clientEndpoint}");
+            clients.Add(client);
+
+            ReturnMessageToClient(client);
+
+            await NetworkStreamHandler(client).ConfigureAwait(false);
+
+            Debug.Log($"<color=red>Disconnected</color> : {clientEndpoint}");
+            clients.Remove(client);
+        }
+
+        void ReturnMessageToClient(TcpClient client)
+        {
+            var terminator = "\r\n";
+
+            byte[] msg = Encoding.GetEncoding("UTF-8").GetBytes($"Established{terminator}");
+            client.GetStream().Write(msg, 0, msg.Length);
+            client.GetStream().Flush();
+        }
+
+        async Task NetworkStreamHandler(TcpClient client)
+        {
+
+            while (client.Connected)
+            {
+                using (var stream = client.GetStream())
+                {
+                    var reader = new StreamReader(stream, Encoding.UTF8);
+
+                    while (!reader.EndOfStream)
+                    {
+                        // TODO : currently, supported string decode only
+                        var str = await reader.ReadLineAsync();
+                        mainContext.Post(_ => OnMessage.Invoke(str), null);
+                    }
+
+                }
+
+            }
+
+            // Disconnected
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
     }
-
 
 }
--- Assets/UnityTCP/Scripts/UnityTCP.cs
+++ Assets/UnityTCP/Scripts/UnityTCP.cs
@@ -1,153 +1,62 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Text;
-using System.IO;
+using UnityEngine.Events;
 
 namespace Kodai100.Tcp
 {
-    
-    class UnityTCP : IDisposable
+
+    public enum SocketType
     {
-        
-        IPEndPoint endpoint;
-
-        TcpListener listener;
-        HashSet<TcpClient> clients = new HashSet<TcpClient>();
-
-        SynchronizationContext mainContext;
-        volatile bool acceptLoop = true;
-        
-        OnMessageEvent OnMessage;
-
-
-        public UnityTCP(IPEndPoint endpoint, OnMessageEvent onMessage)
-        {
-            this.endpoint = endpoint ?? throw new ArgumentNullException("endpoint should not be null");
-
-            // Set Unity main thread
-            mainContext = SynchronizationContext.Current;
-
-            OnMessage = onMessage;
-        }
-        
-        public async Task Listen()
-        {
-            lock (this)
-            {
-                if (listener != null)
-                    throw new InvalidOperationException("Already started");
-
-                acceptLoop = true;
-                listener = new TcpListener(endpoint);
-            }
-
-            listener.Start();
-
-            while (acceptLoop)
-            {
-                try
-                {
-                    var client = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
-                    
-                    var _ = Task.Run(() => OnConnectClient(client));
-
-                }
-                catch (ObjectDisposedException e)
-                {
-                    // thrown if the listener socket is closed
-                }
-                catch (SocketException e)
-                {
-                    // Some socket error
-                }
-            }
-        }
-
-
-
-        public void Stop()
-        {
-
-            lock (this)
-            {
-
-                if (listener == null)
-                    throw new InvalidOperationException("Not started");
-
-                acceptLoop = false;
-
-                listener.Stop();
-                listener = null;
-
-            }
-
-
-            lock (clients)
-            {
-                foreach (var c in clients)
-                {
-                    c.Close();
-                }
-            }
-
-        }
-
-        async Task OnConnectClient(TcpClient client)
-        {
-            var clientEndpoint = client.Client.RemoteEndPoint;
-
-            Debug.Log($"<color=yellow>Established</color> : {clientEndpoint}");
-            clients.Add(client);
-
-            ReturnMessageToClient(client);
-
-            await NetworkStreamHandler(client).ConfigureAwait(false);
-
-            Debug.Log($"<color=red>Disconnected</color> : {clientEndpoint}");
-            clients.Remove(client);
-        }
-
-        void ReturnMessageToClient(TcpClient client)
-        {
-            var terminator = "\r\n";
-
-            byte[] msg = Encoding.GetEncoding("UTF-8").GetBytes($"Established{terminator}");
-            client.GetStream().Write(msg, 0, msg.Length);
-            client.GetStream().Flush();
-        }
-
-        async Task NetworkStreamHandler(TcpClient client)
-        {
-
-            while (client.Connected)
-            {
-                using (var stream = client.GetStream())
-                {
-                    var reader = new StreamReader(stream, Encoding.UTF8);
-
-                    while (!reader.EndOfStream)
-                    {
-                        // TODO : currently, supported string decode only
-                        var str = await reader.ReadLineAsync();
-                        mainContext.Post(_ => OnMessage.Invoke(str), null);
-                    }
-
-                }
-
-            }
-
-            // Disconnected
-        }
-
-        public void Dispose()
-        {
-            Stop();
-        }
+        Server, Client
     }
+
+    public class UnityTCP : MonoBehaviour
+    {
+
+        TCPServer server;
+
+        public SocketType type = SocketType.Server;
+
+        public string ip = "127.0.0.1";
+        public int openPort = 7000;
+
+        public OnMessageEvent OnMessage;
+        
+        void Start()
+        {
+
+            if(type == SocketType.Server)
+            {
+                server = new TCPServer(new IPEndPoint(IPAddress.Any, openPort), OnMessage);
+
+                var t = server.Listen();
+                if (t.IsFaulted) t.Wait();
+            }
+            
+        }
+
+
+        private void OnDisable()
+        {
+
+            if(type == SocketType.Server)
+            {
+                server.Stop();
+            }
+
+            
+        }
+
+    }
+
 
 }
